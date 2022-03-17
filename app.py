fbf3a9dfeb9d3ca1f628425a46d92c6b88f2518c
summerinstockholm <41062566+summerinstockholm@users.noreply.github.com>
2022-03-17 14:19:27 +0300

    add app.py file

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import datetime
+
+# Получаю в переменную значение времени на сейчас и вывожу его на экран с тексто Hello
+def do_magic():
+    now = datetime.datetime.now()
+    return "Hello! {0}".format(now)
+
+# Если мой файл включили как модуль, то он ниче не будет делать.
+if __name__ == "__main__":
+    print(do_magic())
