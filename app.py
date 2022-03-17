caa7e4028191ec86097001ac0c187bef9aa667fe
Vladimir Poroshkov <ammirosat@yandex.ru>
2022-03-17 13:37:20 +0000

    apache2 cgi version

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import datetime
+import os
 
 # Получаю в переменную значение времени на сейчас и вывожу его на экран с тексто Hello
 def do_magic():
@@ -9,5 +10,6 @@ def do_magic():
 
 # Если мой файл включили как модуль, то он ниче не будет делать.
 if __name__ == "__main__":
-    print("Content-type: text/html\n\n")
+    if 'REQUEST_URI' in os.environ:  
+        print("Content-type: text/html\n\n")
     print(do_magic())
