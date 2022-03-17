9b3615eb5464852af299525e4932334c61b5e20c
Vladimir Poroshkov <ammirosat@yandex.ru>
2022-03-17 14:56:11 +0000

    uwsgi version

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ import os
 def do_magic():
     now = datetime.datetime.now()
     return "Hello! {0}".format(now)
-
+# Для uwsgi:
+def application(env, start_response):
+    start_response('200 OK', [('Content-Type','text/html')])
+    return [do_magic()]
 # Если мой файл включили как модуль, то он ниче не будет делать.
 if __name__ == "__main__":
-    if 'REQUEST_URI' in os.environ:  
+    if 'REQUEST_URI' in os.environ:
         print("Content-type: text/html\n\n")
     print(do_magic())
